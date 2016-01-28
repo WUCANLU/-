e2aeacd9293c4bee6cb7cce5e5c32bdb5c3fcb16
WUCANLU <632850375@qq.com>
2016-01-28 20:46:43 +0800

    Create 字符菱形.cpp
    给定一个字符，用它构造一个对角线长5个字符，倾斜放置的菱形

---
+++ week1/字符菱形.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+int main()  {
+       char a;
+       scanf("%c",&a);
+       printf("%3c\n",a);
+	   printf("%2c%c%c\n",a,a,a);
+	   printf("%c%c%c%c%c\n",a,a,a,a,a);
+	   printf("%2c%c%c\n",a,a,a);
+	   printf("%3c",a);
+       return 0;
+}
