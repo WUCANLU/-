24cef6ff81631a89e6ab70d15e7fccdfee75852c
WUCANLU <632850375@qq.com>
2016-02-24 18:41:28 +0800

    Create 正常血压1.cpp
    描述
    监护室每小时测量一次病人的血压，若收缩压在90 - 140之间并且舒张压在60 - 90之间（包含端点值）则称之为正常，现给出某病人若干次测量的血压值，计算病人保持正常血压的最长小时数。
    
    输入
    第一行为一个正整数n，n < 100
    其后有n行，每行2个正整数，分别为一次测量的收缩压和舒张压，中间以一个空格分隔。
    输出
    输出仅一行，血压连续正常的最长小时数。
    样例输入
    4
    100 80
    90 50
    120 60
    140 90
    样例输出
    2

---
+++ week4/正常血压1.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+int main() 
+{
+    int times, D, S, normal_times = 0, max_times = 0;//S==systolic pressure,D==diastolic pressure;
+    scanf("%d", &times);
+     
+     
+    for (int i = 1;i <= times;++i)
+    {
+	  scanf("%d %d", &S, &D);
+    if(S >= 90 && S <= 140 && D >= 60 && D <= 90)
+    {
+       normal_times += 1;
+    }
+    else 
+    {
+	     normal_times = 0;
+    }
+    if(normal_times > max_times)
+    {
+    	 max_times = normal_times;
+    }
+    }
+    
+    
+    printf("%d", max_times);
+    return 0;
+}
